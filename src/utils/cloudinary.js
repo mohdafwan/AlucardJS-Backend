5e91b71dcf64548354bc0a9c3a2a8764ce224eac
3xysha <3xysha@gmail.com>
2024-02-14 13:12:58 +0530

    cloudinary config added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async function (localFilepath) {
+  try {
+    if (!localFilepath) return null;
+    const response = await cloudinary.uploader.upload(localFilepath, {
+      resource_type: 'auto',
+    });
+    console.log('File is uploaded on cloudinary', response.url);
+  } catch (error) {
+    fs.unlinkSync(localFilepath);
+
+    return null;
+  }
+};
